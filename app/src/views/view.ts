491c0e4480abea1b25569aec3f991b70688f36c5
CezarDev <bacanieskidev@gmail.com>
2024-08-12 15:55:20 -0300

    fix: corrigindo reestruturacao

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,26 +1,28 @@
 export abstract class View<T> {
-  private escapar = false;
-  protected elemento: HTMLElement;
 
-  constructor(seletor: string, escapar?: boolean) {
-    const elemento = document.querySelector(seletor);
-    if (elemento) {
-      this.elemento = elemento as HTMLElement;
-    } else {
-      throw Error(`Seletor ${seletor} não existe no dom.`);
-    }
-    if (escapar) {
-      this.escapar = escapar;
-    }
-  }
+    protected elemento: HTMLElement;
+    private escapar = false;
 
-  public update(model: T): void {
-    let template = this.template(model);
-    if (this.escapar) {
-      template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+    constructor(seletor: string, escapar?: boolean) {
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
+        }
+        if (escapar) {
+            this.escapar = escapar;
+        }
     }
-    this.elemento.innerHTML = template;
-  }
 
-  protected abstract template(model: T): string;
+    public update(model: T): void {
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template
+                .replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        this.elemento.innerHTML = template;
+    }
+
+    protected abstract template(model: T): string;
 }
