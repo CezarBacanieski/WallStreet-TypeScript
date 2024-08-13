05e6e5f98160dc5b20b5ef7d82189fb035418270
CezarDev <bacanieskidev@gmail.com>
2024-08-13 12:33:42 -0300

    feat: aplicando cache decorator copy

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,12 +1,14 @@
 export function domInjector(seletor: string) {
   return function (target: any, propertyKey: string) {
+    let elemento: HTMLElement;
     const getter = function () {
-      const elemento = document.querySelector(seletor);
+      if (!elemento) {
+        elemento = <HTMLElement>document.querySelector(seletor);
+      }
+
       return elemento;
     };
 
-    Object.defineProperty(target, propertyKey, {
-      get: getter,
-    });
+    Object.defineProperty(target, propertyKey, { get: getter });
   };
 }
